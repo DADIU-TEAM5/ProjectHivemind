78ccc34f7033c3947ddbd0f23d5ae4bf47c6ecdb
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-10-16 13:18:53 +0200

    Made the MainLoop, a VariantLoop and made a scene that demonstrates it a little bit

---
+++ BugSplat/Assets/Scripts/TestScripts/CubeMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : GameLoop
+{
+    public float MoveLength = 1f;
+    public override void LoopLateUpdate(float deltaTime)
+    {
+    }
+
+    public override void LoopUpdate(float deltaTime)
+    {
+        transform.position += new Vector3(0, 0, MoveLength * deltaTime);
+    }
+}
