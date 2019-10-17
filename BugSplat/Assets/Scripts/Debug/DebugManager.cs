de43e8f3ec824769b59bb7450f492e8ffb23eeb1
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-10-17 10:39:41 +0200

    Added a simple fps counter

---
+++ BugSplat/Assets/Scripts/Debug/DebugManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugManager : MonoBehaviour
+{
+
+    private GUIStyle _overlayStyle = new GUIStyle();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _overlayStyle.fontSize = 40;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        //GUILayout.Label($"FPS: ");
+        GUI.Label(new Rect(40, 40, 200, 80), $"FPS: {1f / Time.deltaTime}", _overlayStyle);
+    }
+}
