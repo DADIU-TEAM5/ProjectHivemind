e8bd2ce99653e4d7630263915a43fb8c114607d6
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-10-17 14:19:47 +0200

    Added the basic pipeline script from last time, and made the cubemovementscene cRaZyyyyyyYYY

---
+++ BugSplat/Assets/Scripts/TestScripts/SpawnManyCubes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManyCubes : GameLoop
+{
+    public GameObject Cube;
+
+    public float SpawnIntervalSeconds = 1f;
+
+    private float _localTimer = 0f;
+
+    private int _column = 0;
+
+    public override void LoopLateUpdate(float deltaTime)
+    {
+    }
+
+    public override void LoopUpdate(float deltaTime)
+    {
+        _localTimer += deltaTime;
+
+        if (_localTimer >= SpawnIntervalSeconds) {
+            var newCube = Instantiate(Cube);
+            newCube.transform.position = Vector3.zero + new Vector3(_column, 0, 0);
+
+            _column++;
+            _column %= 10;
+
+            _localTimer = 0f;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
