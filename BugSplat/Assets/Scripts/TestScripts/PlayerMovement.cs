36f971051a1563346a18d22f70d23a791d99a360
nufanDK <t_a_p@hotmail.com>
2019-10-22 15:32:49 +0200

    PlayerSpeedDirection SO

---
+++ BugSplat/Assets/Scripts/TestScripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Transform Player;
+    public Vector3Variable PlayerDirectionSO;
+
+    [Tooltip("Maximum Speed in m/s")]
+    public float MoveSpeed = 1f;
+    [Tooltip("Acceleration time in seconds")]
+    public float RampupTime = 1f;
+
+
+    private Vector3 _input;
+    private float _lerpTime = 0f;
+    private Vector3 _velocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _input = new Vector3();
+        _velocity = new Vector3();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Assign controller input to _input vector
+        _input.x = Input.GetAxisRaw("Horizontal");
+        _input.y = 0;
+        _input.z = Input.GetAxisRaw("Vertical");
+
+        // SmoothDamp from 0 to 1 on movement
+        if(_input.sqrMagnitude != 0)
+        {
+            Vector3.SmoothDamp(Vector3.zero, _input, ref _velocity, RampupTime);
+        } else
+        {
+            _velocity = Vector3.zero;
+        }
+
+        // Move player using translate
+        Player.Translate(_velocity * MoveSpeed * Time.deltaTime);
+
+    }
+}
---
+++ BugSplat/Assets/Scripts/TestScripts/TouchControls.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControls : MonoBehaviour
+{
+    public Vector3Variable PlayerSpeedDirection;
+    public Transform TouchUIDot;
+
+    private Vector3 _inputTouch;
+    private Touch _touch;
+    private bool _touching = false;
+    private bool _recordMouse = true;
+    private Vector2 _recordedMousePosition;
+    private Vector2 _currentMousePosition;
+    private float _TouchMoveMaxThreshold = 400f;
+    private float _TouchMoveMinThreshold = 100f;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _inputTouch = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Detect Touch
+        if(Input.touchCount > 0)
+        {
+            _touch = Input.GetTouch(0);
+            _touching = true;
+
+            if (_touch.phase == TouchPhase.Moved)
+            {
+                Vector2 touchPosition = _touch.deltaPosition;
+            }
+        }
+
+        // Simulate touch with mouse, if mouse present
+        if(Input.mousePresent)
+        {
+            _currentMousePosition = Input.mousePosition;
+
+            if (Input.GetMouseButton(0))
+            {
+                Debug.Log("MOUSE BUTTON PRESSED!");
+                _touching = true;
+
+                if (_recordMouse)
+                {
+                    _recordedMousePosition = Input.mousePosition;
+                    _recordMouse = false;
+                }
+                
+                // Check if mouse have moved more than the threshold
+                if (Vector2.Distance(_recordedMousePosition, _currentMousePosition) > _TouchMoveMinThreshold) 
+                {
+                    float x = _currentMousePosition.x - _recordedMousePosition.x;
+
+                    PlayerSpeedDirection.Value.x = x;
+
+                    Debug.Log(PlayerSpeedDirection.Value.x);
+
+                }
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                Debug.Log("MOUSEUP DISTANCE: " + Vector2.Distance(_recordedMousePosition, _currentMousePosition));
+                _touching = false;
+                _recordMouse = true;
+            }
+
+        }
+
+
+
+
+
+    }
+
+    private void TRASH()
+    {
+
+    }
+}
