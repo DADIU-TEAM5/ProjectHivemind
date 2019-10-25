0dec2175256dd769742fced13a2a3e681fde6d1c
Mikkel <mikkel.anttila@live.dk>
2019-10-25 14:02:13 +0200

    Added audio scripts for player and enemies, added bank loader script

--- BugSplat/Assets/Scripts/Audio/BankLoaderScript.cs
+++ BugSplat/Assets/Scripts/Audio/BankLoaderScript.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class BankLoaderScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public AK.Wwise.Bank Arena;
+    public AK.Wwise.Bank Hub;
+    public AK.Wwise.Bank Music;
+    public AK.Wwise.Bank UI;
+    public AK.Wwise.Bank Player;
+
+    void Awake()
     {
-        
+        Music.Load(true, false);
+        UI.Load(true, false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnEnterGame()
     {
-        
+        Hub.Load(true, false);
+        Player.Load(true, false);
     }
+
+    public void OnEnterArena()
+    {
+        Hub.Unload();
+        Arena.Load(true, false);
+    }
+
+    public void OnEnterHub()
+    {
+        Arena.Unload();
+        Hub.Load(true, false);
+    }
+
+
+
+
 }
