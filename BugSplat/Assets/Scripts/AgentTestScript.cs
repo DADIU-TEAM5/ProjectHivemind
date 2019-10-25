3d63f6576cd945be021d65c278071027b3f7bf65
sennepsild <magnusmhp@gmail.com>
2019-10-25 15:24:09 +0200

    pathfinding

---
+++ BugSplat/Assets/Scripts/AgentTestScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentTestScript : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+
+    public Transform target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(target.position != navMeshAgent.destination)
+        navMeshAgent.destination = target.position;
+
+
+       // navMeshAgent.Move(Vector3.forward*Time.deltaTime);
+
+    }
+}
