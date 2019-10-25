21815cc711c93afa7bfff94018bfec6b926f1626
Martin Krøll <martino1215@hotmail.com>
2019-10-25 10:25:00 +0200

    Testability Tool added
    Pause button sets timescale to 0

---
+++ BugSplat/Assets/Scripts/TestToolUIControler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestToolUIControler : MonoBehaviour
+{
+    public BoolVariable GameIsPaused;
+    public GameObject TestMenu;
+
+    public void Awake()
+    {
+        TestMenu.SetActive(false);
+    }
+    public void MenuButton()
+    {
+        if (GameIsPaused.Value)
+        {
+            Time.timeScale = 1;
+            GameIsPaused.Value = false;
+            TestMenu.SetActive(false);
+        }
+
+        else
+        {
+            Time.timeScale = 0;
+            GameIsPaused.Value = true;
+            TestMenu.SetActive(true);
+        }
+        
+    }
+}
