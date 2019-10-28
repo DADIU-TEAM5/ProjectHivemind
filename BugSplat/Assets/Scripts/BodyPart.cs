9e21d3f4737e6b5417c6e4261446f6eb47c63792
sennepsild <magnusmhp@gmail.com>
2019-10-28 15:58:55 +0100

    quick math

---
+++ BugSplat/Assets/Scripts/BodyPart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour
+{
+    public Rigidbody Body;
+
+    public float enableTime = 1;
+    float _time = 0;
+    
+    // Start is called before the first frame update
+  
+    private void OnEnable()
+    {
+        //print("BODY PART!");
+
+        Body.AddForce(new Vector3(Random.Range(-15, 15), Random.Range(300,400), Random.Range(-15, 15)));
+    }
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+        if(_time >= enableTime)
+        {
+
+        }
+    }
+
+}
