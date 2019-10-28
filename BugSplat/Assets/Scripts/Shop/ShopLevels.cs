a37009c5873f217d20fb6e2da7354acce71d37c0
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-10-28 15:04:08 +0100

    Started setting up the shop system

---
+++ BugSplat/Assets/Scripts/Shop/ShopLevels.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Shop/Levels")]
+public class ShopLevels : ScriptableObject
+{
+    public TierPicker[] LevelTierPicker;
+}
