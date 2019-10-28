bfaef17c78d5306b524925c241346c37fc5df1b8
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-10-28 13:16:22 +0100

    Added AudioEventListener

---
+++ BugSplat/Assets/Scripts/Events/AudioEventListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AudioEventListener : MonoBehaviour
+{
+    public EventListener[] EventListeners;
+
+    private void Awake()
+    {
+        for (var i = 0; i < EventListeners.Length; i++) {
+            var eventListener = EventListeners[i];
+
+            var listener = gameObject.AddComponent<GameEventListener>();
+           listener.Event = eventListener.Event;
+           listener.Response = eventListener.Response;
+        }        
+    }
+
+    [System.Serializable]
+    public class EventListener {
+        public GameEvent Event;
+        public UnityEvent Response;
+    }
+}
