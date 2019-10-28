351335c1bcb210c5a4b6d94cb944cb3ef5f3aeb9
nufanDK <t_a_p@hotmail.com>
2019-10-28 12:18:11 +0100

    started sceneswitcher

---
+++ BugSplat/Assets/Editor/SceneHandlerEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+[CustomEditor(typeof(SceneHandler))]
+public class SceneHandlerEditor : Editor
+{
+    [SerializeField]
+    private ObjectList _scene;
+    
+    [SerializeField]
+    private Object _selectedScene;
+
+    private string[] _sceneList;
+
+    private void OnEnable()
+    {
+        _sceneList = new string[_scene.Value.Count];
+
+        for (int i = 0; i < _scene.Value.Count; i++)
+        {
+            _sceneList[i] = _scene.Value[i].name;
+        }
+    }
+
+    // OnInspector GUI
+    public override void OnInspectorGUI() //2
+    {
+        EditorGUILayout.Popup(0, _sceneList);
+
+        GUILayout.Space(20f); //2
+        GUILayout.Label("Custom Editor Elements", EditorStyles.boldLabel);
+    }
+
+
+}
---
+++ BugSplat/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    [SerializeField]
+    private ObjectList _scene;
+
+    [SerializeField]
+    private int _selectedScene;
+
+
+    public void ChangeScene(Scene scene)
+    {
+        string sceneName = scene.name;
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
