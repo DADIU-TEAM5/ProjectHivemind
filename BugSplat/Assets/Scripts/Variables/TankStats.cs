4ebdef0dc2bf413de31d513bea3086ee01b9929c
sennepsild <magnusmhp@gmail.com>
2019-10-29 16:02:13 +0100

    tank

---
+++ BugSplat/Assets/Scripts/Variables/TankStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TankStats : ScriptableObject
+{
+    public float MoveSpeed;
+    public float HitPoints;
+    
+
+    public float AttackRange;
+    public float AttackAngle;
+
+
+    public float SpotDistance;
+    
+
+    public float AttackDamage;
+    public float PushLength;
+
+    public int minPartsToDrop;
+    public int maxPartsToDrop;
+
+}
