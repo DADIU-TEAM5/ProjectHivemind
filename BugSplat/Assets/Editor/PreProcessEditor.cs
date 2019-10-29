3e065199add96382952368b6517fa4c40fa617f6
XiJia <bananajiajia@gmail.com>
2019-10-29 15:13:58 +0100

    import anims and script

---
+++ BugSplat/Assets/Editor/PreProcessEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PreProcess))]
+public class PreProcessEditor : Editor
+{
+    private PreProcess _preProcess;
+
+    void OnEnable()
+    {
+        _preProcess = (PreProcess)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+        if (GUILayout.Button("Get Trajectory"))
+        {
+            _preProcess.PreProcessTrajectory();
+        }
+    }
+}
