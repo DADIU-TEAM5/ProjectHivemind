70b9a7a26ba6fefbc1483b646dc03ebaadfabe05
Martin Krøll <martino1215@hotmail.com>
2019-10-30 14:02:47 +0100

    Added Inventory

---
+++ BugSplat/Assets/Editor/ItemManagerEditor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ItemManager))]
+[CanEditMultipleObjects]
+public class ItemManagerEditor : Editor
+{
+
+    ItemManager itemManager;
+    bool testWindowIsOpen = false;
+
+    private void OnEnable()
+    {
+        itemManager = (ItemManager)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUILayout.Space();
+
+        testWindowIsOpen = EditorGUILayout.BeginFoldoutHeaderGroup(testWindowIsOpen, "Item Tests");
+
+        if (testWindowIsOpen)
+        {
+            ItemObject testItem = new ItemObject();
+            
+            if (GUILayout.Button("Add Item"))
+            {
+                Debug.Log("Clicked Add Item");
+                itemManager.AddItem(testItem);
+            }
+
+            if (GUILayout.Button("Reset Items"))
+            {
+                Debug.Log("Clicked Reset");
+                itemManager.ResetItems();
+            }
+
+        }
+
+
+    }
+}
---
+++ BugSplat/Assets/Scripts/Variables/Items/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory")]
+public class Inventory : ScriptableObject
+{
+    public List<ItemObject> Items;
+
+    private void OnEnable()
+    {
+        if (Items==null)
+        Items = new List<ItemObject>();
+    }
+}
