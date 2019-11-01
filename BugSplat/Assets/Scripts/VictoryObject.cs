adc950dbf691e66d7a3fbf09b239966cdcb4e19d
sennepsild <magnusmhp@gmail.com>
2019-11-01 12:01:41 +0100

    revert revert

---
+++ BugSplat/Assets/Scripts/VictoryObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryObject : MonoBehaviour
+{
+    
+    public GameObjectVariable Victory;
+
+    private void OnEnable()
+    {
+        Victory.Value = gameObject;
+    }
+}
