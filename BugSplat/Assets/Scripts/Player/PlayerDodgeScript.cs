9ee0fca429bc0581858a8dd2e212618df9e4605b
nufanDK <t_a_p@hotmail.com>
2019-11-01 15:26:03 +0100

    updated dodgeScript and touch Controls

--- BugSplat/Assets/Scripts/Player/PlayerDodgeScript.cs
+++ BugSplat/Assets/Scripts/Player/PlayerDodgeScript.cs
@@ -6,20 +6,23 @@ using UnityEngine.AI;
 public class PlayerDodgeScript : GameLoop
 {
     public BoolVariable IsDodging;
+
     public Vector3Variable PlayerDirectionSO;
     public Vector3Variable PlayerVelocitySO;
     public FloatVariable DashSpeedSO;
     public FloatVariable DashLengthSO;
-    public AnimationCurve DodgeAnimationCurve;
+    public FloatVariable DashCooldownSO;
+    public AnimationCurve DashAnimationCurve;
 
     private float _currentTime;
     private float _lerpTime = 0f;
     private NavMeshAgent _navMeshAgent;
     private Vector3 _dashDirection;
+    private Vector3 _initialPos;
 
     private void Start()
     {
-        IsDodging.Value = true;
+        IsDodging.Value = false;
         _navMeshAgent = transform.GetComponent<NavMeshAgent>();
     }
 
@@ -28,22 +31,31 @@ public class PlayerDodgeScript : GameLoop
     {
         if (IsDodging.Value == true)
         {
-            Vector3 newPosition = _dashDirection;
+            _lerpTime = Time.time;
+            float _diffTime = _lerpTime - _currentTime;
 
-            if (_lerpTime - _currentTime < DashSpeedSO.Value)
+            float curveTime = DashAnimationCurve.Evaluate(_diffTime/DashSpeedSO.Value);
+            Debug.Log(curveTime);
+
+            if (_diffTime < DashCooldownSO.Value)
             {
-                _lerpTime = Time.time;
-                float _diffTime = _lerpTime - _currentTime;
-                PlayerVelocitySO.Value = Vector3.Lerp(Vector3.zero, _dashDirection * DashLengthSO.Value, _diffTime / DashSpeedSO.Value);
+                if (_diffTime < DashSpeedSO.Value)
+                {
+                    PlayerVelocitySO.Value = Vector3.Lerp(Vector3.zero, _dashDirection * DashLengthSO.Value, curveTime);
+                }
+                else
+                {
+                    PlayerVelocitySO.Value = Vector3.Lerp(Vector3.zero, _dashDirection * DashLengthSO.Value, 1);
+                }
 
-                if(_navMeshAgent.isOnNavMesh)
-                _navMeshAgent.Move(PlayerVelocitySO.Value * DashLengthSO.Value * Time.deltaTime);
-                PlayerDirectionSO.Value = newPosition;
+                if (_navMeshAgent.isOnNavMesh)
+                {
+                    _navMeshAgent.transform.position = _initialPos + PlayerVelocitySO.Value;
+                }
             }
-            else
+            else 
             {
                 IsDodging.Value = false;
-                _lerpTime = 0f;
             }
         }
     }
@@ -63,6 +75,7 @@ public class PlayerDodgeScript : GameLoop
             _dashDirection = PlayerDirectionSO.Value;
             _currentTime = Time.time;
             _lerpTime = 0f;
+            _initialPos = transform.position;
             IsDodging.Value = true;
         }
     }
