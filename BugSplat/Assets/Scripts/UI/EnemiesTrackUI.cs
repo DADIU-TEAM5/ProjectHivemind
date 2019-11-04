ff6eaae009b3df372f8c33578fb51a18666da198
nufanDK <t_a_p@hotmail.com>
2019-11-04 16:58:40 +0100

    added killed and left for the ui

---
+++ BugSplat/Assets/Scripts/UI/EnemiesTrackUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EnemiesTrackUI : GameLoop
+{
+    public GameObjectList EnemiesListSO;
+    public IntVariable EnemiesKilledSO;
+    public TextMeshProUGUI EKills;
+    public TextMeshProUGUI ELeft;
+
+
+    public override void LoopUpdate(float deltaTime)
+    {
+        EKills.text = EnemiesKilledSO.Value.ToString();
+
+        ELeft.text = EnemiesListSO.Items.Count.ToString();
+    }
+
+    public override void LoopLateUpdate(float deltaTime)
+    {
+
+    }
+
+    public void UpdateKills()
+    {
+        EnemiesKilledSO.Value++;
+    }
+}
