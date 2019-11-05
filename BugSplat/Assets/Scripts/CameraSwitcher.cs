129dac9e82dff8577b4eb9dc41a3cd9061f164f0
nufanDK <t_a_p@hotmail.com>
2019-11-05 09:40:33 +0100

    added cameras for the hub and tested cameras for deaths

--- BugSplat/Assets/Scripts/CameraSwitcher.cs
+++ BugSplat/Assets/Scripts/CameraSwitcher.cs
@@ -7,7 +7,9 @@ namespace Cinemachine.Examples
     public class CameraSwitcher : MonoBehaviour
     {
         public List<CinemachineVirtualCamera> VirtualCameras;
-        //public GameObjectVariable CurrentEnemySO;
+        public Transform TankBeetle;
+        public FloatVariable PlayerCurrentSpeedSO;
+        public Vector3Variable PlayerDirectionSO;
         private bool _activate = true;
         private bool _deactivate = false;
 
@@ -35,11 +37,26 @@ namespace Cinemachine.Examples
                 }
             }
 
-            /*if (VirtualCameras[cameraIndex].name == "KillCam")
+            if (VirtualCameras[cameraIndex].name == "KillCamTank")
             {
-                VirtualCameras[cameraIndex].m_LookAt = CurrentEnemySO.Value.transform;
-                Debug.Log(CurrentEnemySO.Value.name);
-            }*/
+                PlayerCurrentSpeedSO.Value = 0;
+                PlayerDirectionSO.Value = Vector3.zero;
+                Time.timeScale = 0.2f;
+                VirtualCameras[cameraIndex].m_LookAt = TankBeetle;
+
+                StartCoroutine(SlowDownTime(cameraIndex, 1f));
+
+            }
+        }
+
+        private IEnumerator SlowDownTime(int cameraIndex, float WaitTime)
+        {
+            yield return new WaitForSeconds(WaitTime);
+
+            Destroy(TankBeetle.gameObject);
+            Time.timeScale = 1f;
+            VirtualCameras[0].gameObject.SetActive(_activate);
+            VirtualCameras[cameraIndex].gameObject.SetActive(_deactivate);
         }
     }
 }
