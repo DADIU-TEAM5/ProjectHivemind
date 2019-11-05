b733f3e0bf07183d08df0e6987e182ebf715f80a
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-11-05 10:41:48 +0100

    Begun reworking the items

---
+++ BugSplat/Assets/Scripts/Items/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory")]
+public class Inventory : ScriptableObject
+{
+    public List<Item> Items;
+
+    public void AddItem(Item itemObj)
+    {
+    }
+    
+    private void OnEnable()
+    {
+        if (Items==null)
+            Items = new List<Item>();
+    }
+}
