71abaeb0e0afa68d9ed77efca6c540b8ee3eb686
sennepsild <magnusmhp@gmail.com>
2019-11-06 15:55:12 +0100

    casper request

---
+++ BugSplat/Assets/Scripts/EnemyCheckOff.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "EnemySpawner/Check List")]
+public class EnemyCheckOff : ScriptableObject
+
+{
+
+    
+
+
+    public EnemyList Enemies;
+
+    [HideInInspector]
+    public bool[] Checks;
+
+
+
+
+    
+}
