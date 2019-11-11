1903ce20a12a5a8bf5e6773df246307f450c569f
Martin Krøll <martino1215@hotmail.com>
2019-11-11 10:39:14 +0100

    Count Aggroed Enemies

---
+++ BugSplat/Assets/Scripts/AggroCounter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AggroCounter : MonoBehaviour
+{
+    public IntVariable AggroedEnemies;
+    public Text AggroTest;
+
+
+
+    public void Start()
+    {
+        AggroedEnemies.Value = 0;
+        AggroTest.text = AggroedEnemies.Value.ToString();
+    }
+
+    public void EnemyKilled()
+    {
+
+        // If PlayerDetected 
+        if (AggroedEnemies.Value > 0)
+            AggroedEnemies.Value--;
+
+        if (AggroTest != null)
+            AggroTest.text = AggroedEnemies.Value.ToString();
+
+
+    }
+
+    public void EnemyAggroed()
+    {
+        AggroedEnemies.Value++;
+        if (AggroTest != null)
+            AggroTest.text = AggroedEnemies.Value.ToString();
+    }
+
+
+}
