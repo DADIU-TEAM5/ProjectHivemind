ce990bc3a96f746d15ae0fe3563c403de76418a9
Martin Krøll <martino1215@hotmail.com>
2019-11-11 15:18:43 +0100

    UnAggro Works kinda...

--- BugSplat/Assets/Scripts/UnAggroManager.cs
+++ BugSplat/Assets/Scripts/UnAggroManager.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class UnAggroManager : GameLoop
 {
     //Use A slower GameLoop for better performance;
-    public EnemyList EnemyList;
+    public AggroCounter AC;
+    public GameObjectList EnemyList;
+    public UpdateManager UM;
+    public IntVariable TimeBeforeResetAggro;
+    
+    private List<int> countdownTimers;
 
+    public void Start()
+    {
+        UM = FindObjectOfType<UpdateManager>();
+        countdownTimers = new List<int>();
+    }
 
     public override void LoopLateUpdate(float deltaTime)
     {
@@ -15,6 +25,47 @@ public class UnAggroManager : GameLoop
 
     public override void LoopUpdate(float deltaTime)
     {
-       foreach(Enemy in EnemyList)
+        int i = 0;
+       foreach(GameObject go in EnemyList.Items)
+        {
+            if (countdownTimers.Count <= i)
+            {
+                countdownTimers.Add(TimeBeforeResetAggro.Value);
+            }
+
+            Enemy enemy = go.GetComponent<Enemy>(); 
+            
+            if(enemy !=null && enemy.PlayerDetected)
+            {
+
+                if (!enemy.IsVisible())
+                {
+                    Debug.Log("Aggroed Enemy is out of vision!");
+
+                    //UnAggro CountDown
+                    countdownTimers[i]--;
+
+                    if (countdownTimers[i] < 1)
+                    {
+
+                        enemy.PlayerDetected = false;
+                        AC.EnemyUnaggro();
+                    }
+
+
+                }
+                else
+                    countdownTimers[i] = TimeBeforeResetAggro.Value;
+                //Vector3 worldPos = Cam.WorldToViewportPoint(enemy.transform.position);
+
+                //if(worldPos.x < 1 && worldPos.x > 0 && worldPos.y < 1 && worldPos.y > 0 && worldPos.z > 0)
+                //{
+                //    // UnAggro CountDown
+                //    Debug.Log("Aggroed Enemy is out of vision!");
+                //}
+
+            }
+            i++;
+        }
     }
 }
