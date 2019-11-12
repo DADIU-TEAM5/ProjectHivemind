59218017b00245edcbba0614fdc373729d3e740d
Martin Krøll <martino1215@hotmail.com>
2019-11-12 11:39:22 +0100

    Currency doesn't reset when changing scene

--- BugSplat/Assets/Scripts/Variables/IntVariable.cs
+++ BugSplat/Assets/Scripts/Variables/IntVariable.cs
@@ -5,7 +5,32 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Variables/Int")]
 public class IntVariable : ScriptableObject
 {
+    // The Current value of the object, used in runtime
     public int Value;
+
+    // Min and Max both used in runtime and in editor
+    public int Min = 0;
+    public int Max = 99;
+
+    // The intial value of the object. This should be adjustable in the editor 
+    public int InitialValue;
+
+
+    public void SetValue(int NewValue)
+    {
+        Value = Mathf.Max(Mathf.Min(NewValue, Max), Min);
+    }
+
+    public void ResetValue()
+    {
+        Value = InitialValue;
+    }
+
+    public void OnEnable()
+    {
+        Value = InitialValue;
+    }
+
 
     public void Increase(int delta) {
         Value += delta;
