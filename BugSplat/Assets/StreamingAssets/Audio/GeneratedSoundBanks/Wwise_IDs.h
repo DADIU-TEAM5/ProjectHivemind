9a330889d79d4426b7e6e338248dad9f19041bdd
Mikkel <mikkel.anttila@live.dk>
2019-11-12 11:05:38 +0100

    added boomer footsteps

--- BugSplat/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ BugSplat/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -46,6 +46,7 @@ namespace AK
         static const AkUniqueID NPC_HEAVY_SPECIALATTACK = 575616738U;
         static const AkUniqueID NPC_SPITTER_ATTACK = 1630111423U;
         static const AkUniqueID NPC_SPITTER_BURROW = 141041902U;
+        static const AkUniqueID NPC_SPITTER_CHARGE = 2415521115U;
         static const AkUniqueID NPC_SPITTER_DEAD = 1581237869U;
         static const AkUniqueID NPC_SPITTER_DETECT = 2631762934U;
         static const AkUniqueID NPC_SPITTER_FOOTSTEPS = 3732201856U;
