7bb9bfc2193d4735563deb5f55aad01188b47565
sennepsild <magnusmhp@gmail.com>
2019-11-14 11:28:46 +0100

    everything is cutup

---
+++ BugSplat/Assets/Editor/BodypartEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(BodyPart))]
+[CanEditMultipleObjects]
+public class BodypartEditor : Editor
+{
+    BodyPart part;
+
+    private void OnEnable()
+    {
+        part = (BodyPart)target;
+
+        part.Collider = part.gameObject.GetComponent<Collider>();
+        part.Body = part.gameObject.GetComponent<Rigidbody>();
+
+
+        
+    }
+
+
+}
