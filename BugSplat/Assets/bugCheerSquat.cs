726ce96b222f3357b19b78808277dc84d3e788dc
Mikkel <mikkel.anttila@live.dk>
2019-11-18 14:24:37 +0100

    Added ph cheer

---
+++ BugSplat/Assets/bugCheerSquat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bugCheerSquat : MonoBehaviour
+{
+    [Header("Wwise events")]
+    public AK.Wwise.Event WinningCheer;
+    public AK.Wwise.Event LosingCheer;
+    public AK.Wwise.Event ComboCheer;
+
+    public void HasWon()
+    {
+        WinningCheer.Post(this.gameObject);
+    }
+
+    public void HasLost()
+    {
+        LosingCheer.Post(this.gameObject);
+    }
+
+    public void Combo()
+    {
+        ComboCheer.Post(this.gameObject);
+    }
+}
