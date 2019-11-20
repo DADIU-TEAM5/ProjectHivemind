44a1ae88e2fb08b92040c47b48c98a964cbea746
nufanDK <t_a_p@hotmail.com>
2019-11-20 09:36:53 +0100

    fixed tutorial parts

--- BugSplat/Assets/Scripts/Camera/FreezePlayerPositionToPoint.cs
+++ BugSplat/Assets/Scripts/Camera/FreezePlayerPositionToPoint.cs
@@ -6,11 +6,24 @@ public class FreezePlayerPositionToPoint : MonoBehaviour
 {
     public Transform PlayerGameObject;
     public Transform TargetPosition;
+    public FloatVariable PlayerCurrentSpeedSO;
+    public Vector3Variable PlayerDirectionSO;
     
 
     // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
+        PlayerCurrentSpeedSO.InitialValue = 0;
+        PlayerDirectionSO.Value = Vector3.zero;
         PlayerGameObject.position = TargetPosition.position;
+        PlayerGameObject.parent = TargetPosition;
+        PlayerGameObject.GetComponentInChildren<PlayerTrajectory>().MoMaUpdateTime = 100;
+
+    }
+
+    public void Reset()
+    {
+        PlayerGameObject.parent = null;
+        PlayerGameObject.GetComponentInChildren<PlayerTrajectory>().MoMaUpdateTime = 0.2f;
     }
 }
