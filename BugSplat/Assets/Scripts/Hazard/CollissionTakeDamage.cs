32a68d07554b2863eb4f9de7fe9194ed2596465a
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-11-20 14:08:24 +0100

    Fairplay spikes

---
+++ BugSplat/Assets/Scripts/Hazard/CollissionTakeDamage.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollissionTakeDamage : MonoBehaviour
+{
+    public float PlayerDamage, EnemyDamage;
+
+    public GameEvent OnDamageEvent;
+
+    public bool PlayerTakeDamage(GameObject gameObject) {
+        var player = gameObject.GetComponent<PlayerHealth>();
+            
+        if (player != null) {
+            Debug.Log("player take damage");
+            player.TakeDamage(PlayerDamage);
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool EnemyTakeDamage(GameObject gameObject) {
+        var potentialEnemy = gameObject.GetComponent<Enemy>();
+
+        if (potentialEnemy != null) {
+            Debug.Log("enemy take damage");
+            potentialEnemy.TakeDamage(EnemyDamage);
+            return true;
+        }
+
+        return false;
+    }
+    
+    void OnTriggerEnter(Collider collision) {
+        if (collision == null) return;
+
+        if (!EnemyTakeDamage(collision.gameObject)) {
+            PlayerTakeDamage(collision.gameObject);
+        }
+    }
+}
