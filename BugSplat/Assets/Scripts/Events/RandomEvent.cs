021312984395549b1c645840ec748b8e8af5f2af
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-11-21 16:04:17 +0100

    Progress

--- BugSplat/Assets/Scripts/Events/RandomEvent.cs
+++ BugSplat/Assets/Scripts/Events/RandomEvent.cs
@@ -9,7 +9,9 @@ public class RandomEvent : MonoBehaviour
     public void RaiseRandom() {
         if (Events == null || Events.Length == 0) return;
 
+        Debug.Log(Events.Length);
         var randomIndex = Random.Range(0, Events.Length);
+        Debug.Log(randomIndex);
 
         Events[randomIndex]?.Raise();
     }
