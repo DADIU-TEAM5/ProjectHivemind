aebf5d8464ef3e64197d19612b7e190c256d7d25
nufanDK <t_a_p@hotmail.com>
2019-11-21 15:41:28 +0100

    added spawn animations to the boomer

--- BugSplat/Assets/Scripts/TestScripts/DeParentObject.cs
+++ BugSplat/Assets/Scripts/TestScripts/DeParentObject.cs
@@ -8,6 +8,13 @@ public class DeParentObject : MonoBehaviour
 
     public void DeParent(int index)
     {
+        Vector3 currentWorldPos = ObjectChilds[index].transform.position;
+
+        Debug.Log("PArent: " + ObjectChilds[index].transform.parent.position);
+        Debug.Log("Child: " + ObjectChilds[index].transform.position);
+
+        ObjectChilds[index].transform.position = currentWorldPos;
+
         ObjectChilds[index].transform.parent = null;
     }
 }
