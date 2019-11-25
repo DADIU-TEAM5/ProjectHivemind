4d2f6c3328ae71159b438641bac0e2b880521355
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-11-25 14:10:25 +0100

    Fixed projectile effect direction

--- BugSplat/Assets/Editor/GameEventEditor.cs
+++ BugSplat/Assets/Editor/GameEventEditor.cs
@@ -6,12 +6,16 @@ using UnityEditor;
 [CustomEditor(typeof(GameEvent))]
 public class GameEventEditor : Editor
 {
+    public GameObject Target;
+
     public override void OnInspectorGUI() {
         base.OnInspectorGUI();
 
         var gameEvent = (GameEvent) target;
+
+        Target = (GameObject) EditorGUILayout.ObjectField(Target, typeof(GameObject), allowSceneObjects: true);
         if (GUILayout.Button("Raise Event!")) {
-            gameEvent.Raise(); 
+            gameEvent.Raise(Target); 
         }
     }
 }
