a7ce7b6207b8a62654955cfca91e55dcfc002877
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-11-27 15:01:09 +0100

    Assetbundle stuff

---
+++ BugSplat/Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using System.IO;
+using UnityEngine;
+
+public class CreateAssetBundles 
+{
+    [MenuItem("Assets/ Build AssetBundles")]
+    static void BuildAllAssetBundles() {
+        var assetBundleDirectory = "Assets/AssetBundles";
+
+        if (!Directory.Exists(assetBundleDirectory)) Directory.CreateDirectory(assetBundleDirectory);
+
+        var manifest = BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.StandaloneWindows64);
+
+        if (manifest == null) {
+            Debug.LogError("Manifest is null, asset budles big errr");
+        }
+    }
+}
---
+++ BugSplat/Assets/Scripts/AssetBundles/AssetBundleManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class AssetBundleManager : MonoBehaviour
+{
+    public string AssetBundleURL;
+
+    public GameEvent BundledLoaded;
+
+    protected AssetBundle _loadedBundle;
+
+
+    virtual protected IEnumerator Start() {
+        yield return LoadBundle();
+
+        LoadAllAssets();
+
+        DontDestroyOnLoad(transform.root.gameObject);
+    }
+
+    public IEnumerator LoadBundle() {
+        var request = UnityWebRequestAssetBundle.GetAssetBundle(AssetBundleURL);
+        yield return request.SendWebRequest();
+
+        _loadedBundle = DownloadHandlerAssetBundle.GetContent(request);
+    }
+
+    public void UnloadBundle() {
+        _loadedBundle.Unload(true);
+    }
+
+    public void LoadAllAssets() {
+        var objects = _loadedBundle.LoadAllAssets();
+
+        BundledLoaded?.Raise(gameObject);
+    }
+
+    public T GetAsset<T>(string assetName) where T : UnityEngine.Object {
+        return _loadedBundle.LoadAsset<T>(assetName);
+    }
+
+    void OnDestroy() {
+        _loadedBundle.Unload(true);
+    }
+}
