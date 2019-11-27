7a523ba70853067a90f9f74e6c2f582b14646592
xijia <bananajiaaa@163.com>
2019-11-27 19:25:36 +0100

    outcomes

--- BugSplat/Assets/Scripts/UI/WaitclikOrTap.cs
+++ BugSplat/Assets/Scripts/UI/WaitclikOrTap.cs
@@ -7,7 +7,7 @@ public class WaitclikOrTap : MonoBehaviour
     public GameObject Scenehandle;
 	void Update()
 	{
-		if (Input.GetMouseButtonUp(0))
+		if (Input.GetMouseButtonUp(0) || Input.touchCount > 0)
 		{
 			Scenehandle.SetActive(true);
 		}
