93d6204fadbe062d64578eb06292c2fbef52d73d
XiJia <bananajiajia@gmail.com>
2019-11-27 14:07:28 +0100

    save states

---
+++ BugSplat/Assets/Scripts/UI/CountAllResults.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountAllResults : MonoBehaviour
+{
+    public FloatVariable LevelTimeConsume;
+    public FloatVariable TotalTimeConsume;
+    public IntVariable TotalEarned;
+    public IntVariable LevelEarned;
+
+    public IntVariable TotalEnemiesKilled;
+    public IntVariable LevelEnemiesKilled;
+
+
+    public void CountEnemiesKilled()
+    {
+        LevelEnemiesKilled.Value += 1;
+        TotalEnemiesKilled.Value += 1;
+    }
+
+    public void LevelClearLoadAfterShowing()
+    {
+        LevelEarned.Value = 0;
+        TotalEnemiesKilled.Value += LevelEnemiesKilled.Value;
+        LevelEnemiesKilled.Value = 0;
+        LevelTimeConsume.Value = 0;
+    }
+
+    public void PlayerDiedLoadAfterShowing()
+    {
+        TotalEnemiesKilled.Value = 0;
+        TotalTimeConsume.Value = 0;
+        TotalEarned.Value = 0;
+    }
+}
