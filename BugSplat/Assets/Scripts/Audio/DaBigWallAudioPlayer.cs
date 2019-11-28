c0b0e5f7100cfb6751afaea04d58a5f95e9398af
Mikkel <mikkel.anttila@live.dk>
2019-11-28 13:55:33 +0100

    Added sounds fixed bug where ambience from hub played in arena

---
+++ BugSplat/Assets/Scripts/Audio/DaBigWallAudioPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaBigWallAudioPlayer : MonoBehaviour
+{
+    public AK.Wwise.Event WallCloseOpen;
+
+
+    public void WallCloseOpenEvent(GameObject source)
+    {
+        WallCloseOpen.Post(source);
+    }
+}
