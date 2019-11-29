443a672a798d8175182ddce42e8684b591ca02f8
nufanDK <t_a_p@hotmail.com>
2019-11-29 12:30:34 +0100

    added spawnanimation for player

--- BugSplat/Assets/Scripts/TestScripts/LoadTutorialShop.cs
+++ BugSplat/Assets/Scripts/TestScripts/LoadTutorialShop.cs
@@ -20,6 +20,8 @@ public class LoadTutorialShop : MonoBehaviour
     public GameObject TutorialEggs;
     public GameObject[] LevelLights;
     public GameObject ShopSign;
+    public GameObject SpawnParticle;
+    public Vector3Variable PlayerDirectionSO;
 
 
 
@@ -59,12 +61,13 @@ public class LoadTutorialShop : MonoBehaviour
     
     public void  StartGame()
     {
-        
+
         
 
         if (TutorialIsActiveSO.Value == true)
         {
-            
+            Player.gameObject.SetActive(true);
+            Player.GetComponent<NavMeshAgent>().enabled = false;
             ShopIsOpenSO.Value = false;
             Cage.SetActive(true);
             //
@@ -73,11 +76,20 @@ public class LoadTutorialShop : MonoBehaviour
             TutorialEggs.SetActive(true);
         } else
         {
+            if (LastSceneSO.Value == "_PreloadScene")
+            {
+                SpawnParticle.SetActive(true);
+                SpawnParticle.GetComponent<WaitXSeconds>().enabled = true;
+            } else
+            {
+                SpawnParticle.SetActive(true);
+                Player.gameObject.SetActive(true);
+            }
+
             ShopIsOpenSO.Value = true;
             TutorialIsActiveSO.Value = false;
             TutorialEggs.SetActive(false);
             PlayerControlOverrideSO.Value = false;
-            Player.GetComponent<NavMeshAgent>().enabled = true;
         }
 
         if (ShopIsOpenSO.Value == true)
@@ -87,9 +99,6 @@ public class LoadTutorialShop : MonoBehaviour
         {
             ShopSign.SetActive(true);
         }
-
-    
-
-        
+       
     }
 }
