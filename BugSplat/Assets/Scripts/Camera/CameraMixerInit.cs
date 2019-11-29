2174884c07f4f4c854ea70e64accbc8e82c81f31
nufanDK <t_a_p@hotmail.com>
2019-11-29 10:31:14 +0100

    fixed some stuff outrocamera etc

---
+++ BugSplat/Assets/Scripts/Camera/CameraMixerInit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraMixerInit : MonoBehaviour
+{
+
+    public CinemachineMixingCamera MixerCam;
+    public Transform PlayerGameObject;
+    public Transform InitTarget;
+    public Transform DestTarget;
+
+    private float _initDist;
+    private float _currentDist;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _initDist = Vector3.Distance(InitTarget.position, DestTarget.position);
+        //Debug.Log("INITDIST: " + _initDist);
+
+        MixerCam.m_Weight0 = 1;
+        MixerCam.m_Weight1 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentDist = Vector3.Distance(PlayerGameObject.position, DestTarget.position)/_initDist;
+
+        //Debug.Log("CurrentDist: " + _currentDist);
+
+        if (_currentDist < 1)
+        {
+            MixerCam.m_Weight0 = _currentDist;
+            MixerCam.m_Weight1 = 1 - _currentDist;
+        }
+    }
+}
