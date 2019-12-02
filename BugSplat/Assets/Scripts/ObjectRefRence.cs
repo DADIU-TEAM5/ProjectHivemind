01654ff8429d89130dd7a9788a9eb754a71e6796
sennepsild <magnusmhp@gmail.com>
2019-12-02 15:01:05 +0100

    all the stuff

---
+++ BugSplat/Assets/Scripts/ObjectRefRence.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRefRence : MonoBehaviour
+{
+
+    public Camera cam;
+    float camSize = 20;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        cam.orthographicSize = camSize;
+
+        while (Screen.height / cam.WorldToScreenPoint(transform.position).y > 1.10f) 
+        {
+            camSize -= 0.1f;
+
+            cam.orthographicSize = camSize;
+        }
+        yield return null;
+    }
+
+
+
+}
