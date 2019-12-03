cd36ffe2c017eb3dea3fa5714d6bd6aaeb829cac
Martin Krøll <martino1215@hotmail.com>
2019-12-03 15:40:57 +0100

    New Font Added

---
+++ BugSplat/Assets/Editor/ChangeFontScript.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace UnityEditor
+{
+
+    public class OpenFilePanelExample : Editor
+    {
+
+        public static TMPro.TMP_FontAsset TextFont = null;
+       
+        public static void Apply()
+        {
+           
+            
+         
+            Debug.Log(TextFont.name);
+
+            foreach (TMPro.TextMeshProUGUI text in FindObjectsOfType<TMPro.TextMeshProUGUI>())
+            {
+                Debug.Log("Changed Font");
+                text.font = TextFont;
+
+            }
+        }
+    }
+
+}
