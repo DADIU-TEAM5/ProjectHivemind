55273dd21eb5292f3af7dae1bb59435954d38901
sennepsild <magnusmhp@gmail.com>
2019-12-04 15:19:15 +0100

    on by default

--- BugSplat/Assets/Scripts/PosProcessingToggleScript.cs
+++ BugSplat/Assets/Scripts/PosProcessingToggleScript.cs
@@ -20,7 +20,7 @@ public class PosProcessingToggleScript : GameLoop
 
         int intvalue = PlayerPrefs.GetInt("PostProcessingToggle");
 
-        if (intvalue == 1)
+        if (intvalue == 0)
             Toggle.Value = true;
         else
             Toggle.Value = false;
