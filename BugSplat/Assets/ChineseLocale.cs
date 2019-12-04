ce6e8a4a8812786598f177e14aa1558102e28cea
XiJia <bananajiajia@gmail.com>
2019-12-04 16:11:01 +0100

    initial

---
+++ BugSplat/Assets/ChineseLocale.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChineseLocale : MonoBehaviour
+{
+    public TMPro.TMP_FontAsset ChineseFont;
+    public LocaleVariable CurrentLocale;
+    public Locale Chinese;
+    public void ChangeToChinese(TMPro.TextMeshProUGUI textMesh)
+    {
+        if (CurrentLocale.Value == Chinese)
+        {
+            textMesh.font = ChineseFont;
+        }
+    }
+
+
+}
