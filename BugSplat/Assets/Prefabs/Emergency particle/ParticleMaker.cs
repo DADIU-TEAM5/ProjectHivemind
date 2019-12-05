7ae797e5b62f2b9ab59a819cf9ad5fa9fec6c2e0
sennepsild <magnusmhp@gmail.com>
2019-12-05 22:42:12 +0100

    Destroy and instantiate dash stuff

---
+++ BugSplat/Assets/Prefabs/Emergency particle/ParticleMaker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleMaker : MonoBehaviour
+{
+
+    public GameObject[] ParticlesToSpawn;
+
+    public void SpawnParticles()
+    {
+
+        for (int i = 0; i < ParticlesToSpawn.Length; i++)
+        {
+           GameObject particle =  Instantiate(ParticlesToSpawn[i], transform);
+
+            Destroy(particle, 0.5f);
+        }
+
+    }
+
+
+}
