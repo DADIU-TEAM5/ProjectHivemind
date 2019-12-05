ee4149a537c2b47349c361ad16025774bbddc83a
nufanDK <t_a_p@hotmail.com>
2019-12-05 15:18:44 +0100

    Update cannonFodder.cs

--- BugSplat/Assets/Scripts/cannonFodder.cs
+++ BugSplat/Assets/Scripts/cannonFodder.cs
@@ -91,89 +91,92 @@ public class cannonFodder : Enemy
 
     void Attack()
     {
-       
-        
 
-        if (_attacking == false && !IsUnderground)
+
+
+        if (!IsUnderground)
         {
-            AttackChargingEvent.Raise(this.gameObject);
-
-            Vector3 adjustedPlayerPos = PlayerTransform.position;
-
-            adjustedPlayerPos.y = transform.position.y;
-
-            transform.LookAt(adjustedPlayerPos);
-
-            Cone.SetActive(true);
-            Outline.SetActive(true);
-            OuterEdge.SetActive(true);
-
-            FodderAnimator.SetTrigger("Attack");
-            FodderAnimator.speed = 1 + _percentIncrease;
-
-            DrawCone(4, OutlineMesh, true,_attackCharge);
-            DrawCone(4, OuterEdgeMesh, true, _attackCharge);
-
-        }
-        DrawCone(5,ConeMesh,false,_attackCharge);
-
-        ConeRenderer.material.color = Color.Lerp(ConeInitColor, ConeEndColor, _attackCharge / stats.AttackChargeUpTime);
-        //Color.Lerp(new Color(0,1,0,0.5f), new Color(1, 0, 0, 0.5f), _attackCharge / stats.AttackChargeUpTime);
-
-
-        _attacking = true;
-        _attackCharge += Time.deltaTime;
-
-        if (_attackCharge >= stats.AttackChargeUpTime)
-        {
-            AttackEvent.Raise(this.gameObject);
-            Collider[] potentialTargets = Physics.OverlapSphere(transform.position, stats.AttackRange, LayerMask.GetMask("Player"));
-
-            RaycastHit hit;
-            if (potentialTargets.Length>0 && Physics.Raycast(transform.position, potentialTargets[0].transform.position - transform.position, out hit, 10))
+            if (_attacking == false)
             {
-                if (hit.collider.gameObject.layer == 9)
-                {
+                AttackChargingEvent.Raise(this.gameObject);
 
-                    
-                    Vector3 temp = potentialTargets[0].transform.position;
-                    temp.y = transform.position.y;
+                Vector3 adjustedPlayerPos = PlayerTransform.position;
 
+                adjustedPlayerPos.y = transform.position.y;
 
-                    
-                    if (Vector3.Angle(transform.position - (transform.position + transform.forward), transform.position - temp) < stats.AttackAngle)
-                    {
-                        PlayerHealth playerHealth = potentialTargets[0].GetComponent<PlayerHealth>();
-                        //apply damage to the player
-                        if (playerHealth != null)
-                        {
-                            Vector3 directionToPush = potentialTargets[0].gameObject.transform.position - transform.position;
-                            directionToPush.y = 0;
-                            directionToPush = Vector3.Normalize(directionToPush);
+                transform.LookAt(adjustedPlayerPos);
 
-                            playerHealth.TakeDamage( stats.AttackDamage);
-                        }
-                        else
-                        {
-                            Debug.LogError("target of " + gameObject.name + " attack got no health");
-                        }
-                    }
-                }
-                else
-                    print("attack blocked by terrain or something");
+                Cone.SetActive(true);
+                Outline.SetActive(true);
+                OuterEdge.SetActive(true);
+
+                FodderAnimator.SetTrigger("Attack");
+                FodderAnimator.speed = 1 + _percentIncrease;
+
+                DrawCone(4, OutlineMesh, true, _attackCharge);
+                DrawCone(4, OuterEdgeMesh, true, _attackCharge);
 
             }
-            else
-                print("this should never show i guess");
+            DrawCone(5, ConeMesh, false, _attackCharge);
 
-            _attackCooldown = stats.AttackSpeed;
-            _attacking = false;
-            _attackCharge = 0;
-            Cone.SetActive(false);
-            Outline.SetActive(false);
-            OuterEdge.SetActive(false);
-            FodderAnimator.speed = 1 ;
+            ConeRenderer.material.color = Color.Lerp(ConeInitColor, ConeEndColor, _attackCharge / stats.AttackChargeUpTime);
+            //Color.Lerp(new Color(0,1,0,0.5f), new Color(1, 0, 0, 0.5f), _attackCharge / stats.AttackChargeUpTime);
 
+
+            _attacking = true;
+            _attackCharge += Time.deltaTime;
+
+            if (_attackCharge >= stats.AttackChargeUpTime)
+            {
+                AttackEvent.Raise(this.gameObject);
+                Collider[] potentialTargets = Physics.OverlapSphere(transform.position, stats.AttackRange, LayerMask.GetMask("Player"));
+
+                RaycastHit hit;
+                if (potentialTargets.Length > 0 && Physics.Raycast(transform.position, potentialTargets[0].transform.position - transform.position, out hit, 10))
+                {
+                    if (hit.collider.gameObject.layer == 9)
+                    {
+
+
+                        Vector3 temp = potentialTargets[0].transform.position;
+                        temp.y = transform.position.y;
+
+
+
+                        if (Vector3.Angle(transform.position - (transform.position + transform.forward), transform.position - temp) < stats.AttackAngle)
+                        {
+                            PlayerHealth playerHealth = potentialTargets[0].GetComponent<PlayerHealth>();
+                            //apply damage to the player
+                            if (playerHealth != null)
+                            {
+                                Vector3 directionToPush = potentialTargets[0].gameObject.transform.position - transform.position;
+                                directionToPush.y = 0;
+                                directionToPush = Vector3.Normalize(directionToPush);
+
+                                playerHealth.TakeDamage(stats.AttackDamage);
+                            }
+                            else
+                            {
+                                Debug.LogError("target of " + gameObject.name + " attack got no health");
+                            }
+                        }
+                    }
+                    else
+                        print("attack blocked by terrain or something");
+
+                }
+                else
+                    print("this should never show i guess");
+
+                _attackCooldown = stats.AttackSpeed;
+                _attacking = false;
+                _attackCharge = 0;
+                Cone.SetActive(false);
+                Outline.SetActive(false);
+                OuterEdge.SetActive(false);
+                FodderAnimator.speed = 1;
+
+            }
         }
         
     }
