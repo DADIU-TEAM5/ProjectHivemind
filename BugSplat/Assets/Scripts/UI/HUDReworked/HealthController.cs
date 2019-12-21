d19833a8d7124a63ccd8cc954716e7ccbf86ac4a
Oliver Jørgensen <oljo@itu.dk>
2019-12-21 18:13:32 +0100

    Reworked health ui. Needs polish.

---
+++ BugSplat/Assets/Scripts/UI/HUDReworked/HealthController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    public HealthPointController[] HealthPoints;
+
+    public FloatVariable CurrentHealth;
+    public FloatVariable MaxHealth;
+
+    public FloatVariable HealthPerPoint;
+
+    [PostProcessScene]
+    private void BuildAwake() {
+        HealthPerPoint.Value = MaxHealth.Max / HealthPoints.Length;
+    }
+
+    private void Awake() {
+        UpdateHealth();
+    }
+
+    public void UpdateHealth() {
+        var currentHealth = CurrentHealth.Value;
+        var maxHealth = MaxHealth.Value;
+        var fullHealthPoint = 0f;
+
+        for (var i = 0; i < HealthPoints.Length; i++) {
+            var healthPoint = HealthPoints[i];
+
+            if (maxHealth > fullHealthPoint) {
+                healthPoint.ShowPoint();
+
+                if (currentHealth > fullHealthPoint) {
+                    var diff = currentHealth - fullHealthPoint;
+                    if (diff >= HealthPerPoint.Value) {
+                        healthPoint.SetFillAmount(1f);
+                    } else {
+                        healthPoint.SetFillAmount(diff / HealthPerPoint.Value);
+                    }
+                } else {
+                    healthPoint.SetFillAmount(0f);
+                }
+
+                fullHealthPoint += HealthPerPoint.Value;
+            } else {
+                healthPoint.HidePoint();
+            }
+        }
+    }
+}
---
+++ BugSplat/Assets/Scripts/UI/HUDReworked/HealthPointController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthPointController : MonoBehaviour
+{
+    public Canvas Canvas;
+    public Image HealthImage;
+
+    public void ShowPoint() {
+        Canvas.enabled = true;
+    }
+    
+    public void HidePoint() {
+        Canvas.enabled = false;
+    }
+
+    public void SetFillAmount(float fillAmount) {
+        HealthImage.fillAmount = fillAmount;
+    }
+}
