f408f1efb5d1f3add950234870c5976bb919902b
Oliver Jørgensen <oljo@itu.dk>
2019-12-26 01:51:31 +0100

    Rewrote the mapgenerator

--- BugSplat/Assets/Scripts/MapController.cs
+++ BugSplat/Assets/Scripts/MapController.cs
@@ -18,11 +18,13 @@ public class MapController : MonoBehaviour, IPreprocessBuildWithReport
     public bool UseRandomSeed;
     public int Seed;
 
+    public GameObject EdgeWall;
+    public GameObject WallConnector, SideWallConnector;
+    
+
     [SerializeField]
     public Dictionary<Tier, List<Hexagon>> TierToHexDic;
 
-    [SerializeField]
-    private Vector3[] _hexPositions;
     [SerializeField]
     private Quaternion[] _hexRotations;
 
@@ -33,6 +35,8 @@ public class MapController : MonoBehaviour, IPreprocessBuildWithReport
     private GameObject BaseHex;
 
     private float HexHeight;
+
+    private Hexagon[] MapHexagons;
 
     [Header("Events")]
     public GameEvent GauntletEvent;
@@ -50,7 +54,6 @@ public class MapController : MonoBehaviour, IPreprocessBuildWithReport
                 var hexagonList = TierToHexDic[tier];
                 hexagonList.Add(hexagon);
             } else {
-                Debug.Log($"New tier: {tier.name}");
                 TierToHexDic.Add(tier, new List<Hexagon>() { hexagon });
             }
         }
@@ -61,16 +64,7 @@ public class MapController : MonoBehaviour, IPreprocessBuildWithReport
         var baseHexRenderer = BaseHex.GetComponent<MeshRenderer>();
         var bounds = baseHexRenderer.bounds;
         HexHeight = bounds.size.z;
-
-        _hexPositions = new Vector3[] {
-            new Vector3(0f, 0f, bounds.size.z),
-            new Vector3(0f, 0f, -bounds.size.z),
-            new Vector3(bounds.size.x, 0f, bounds.size.z * 0.5f),
-            new Vector3(bounds.size.x, 0f, bounds.size.z * 0.5f),
-            new Vector3(bounds.size.x, 0f, -bounds.size.z * 0.5f),
-            new Vector3(bounds.size.x, 0f, -bounds.size.z * 0.5f),
-        };
-
+        
         _hexRotations = new Quaternion[6];
         for (int i = 0; i < _hexRotations.Length; i++) {
             var angle = 180 + (60 * i);
@@ -92,13 +86,73 @@ public class MapController : MonoBehaviour, IPreprocessBuildWithReport
         if (!isGauntlet) {
             OuterTiles = WaveTiles;
             CenterTiles = WaveCenterTiles;
+            WaveEvent?.Raise();
+        } else {
+            GauntletEvent?.Raise();
         }
         
         var centerHex = Instantiate(GetRandomCenterHexagon(), Vector3.zero, Quaternion.identity, _parentObject.transform);
 
-        for (int i = 0; i < _hexPositions.Length; i++) {
+        MapHexagons = new Hexagon[6];
+        for (int i = 0; i < MapHexagons.Length; i++) {
             var hex = Instantiate(GetHexagonBasedOnLevel(), Vector3.zero, _hexRotations[i], _parentObject.transform);
+
             hex.transform.localPosition += hex.transform.forward * HexHeight; 
+            hex.DistributeBudget();
+
+            MapHexagons[i] = hex;
+        }
+
+        if (!isGauntlet) RotateAllPossibleSolutions();
+    }
+
+    private void RotateTilesToMakeMostPossibleConnections()
+    {
+
+        RotateAllPossibleSolutions();
+        for (int i = 0; i < MapHexagons.Length; i++)
+        {
+            var hex = MapHexagons[i];
+
+            if (!hex.IsaccesibleFromMiddle)
+            {
+                hex.OpenAndRotateNeighbour();
+                RotateAllPossibleSolutions();
+            }
+            
+        }
+
+        for (int i = 0; i < MapHexagons.Length; i++)
+        {
+            var hex = MapHexagons[i];
+            hex.RemoveOuterWalls();
+        }
+
+        for (int i = 0; i < MapHexagons.Length; i++)
+        {
+            var hex = MapHexagons[i];
+            hex.CreateConnectors();
+        }
+    }
+
+    void RotateAllPossibleSolutions()
+    {
+        bool noSolution = false;
+        while (!noSolution)
+        {
+            noSolution = true;
+
+            for (int i = 0; i < MapHexagons.Length; i++)
+            {
+                var hex = MapHexagons[i];
+
+                if (hex.HasAMiddleConnectedNeighbourWithAnOpenSideTowardsThisTile() && !hex.IsaccesibleFromMiddle)
+                {
+                    noSolution = false;
+
+                    hex.RotateToFitMiddleConnectedNeighbour();
+                }
+            }
         }
     }
 
